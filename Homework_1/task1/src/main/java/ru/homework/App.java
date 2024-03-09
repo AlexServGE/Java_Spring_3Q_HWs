8a15af7e249b19eb078c81ddbb34798361405343
AlexServGE <alexander.sergeevru@yandex.ru>
2024-03-09 17:14:42 +0300

    HW1 is completed

---
+++ Homework_1/task1/src/main/java/ru/homework/App.java
@@ -0,0 +1,14 @@
+package ru.homework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+}
